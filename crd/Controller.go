b6021d532d7844681f67f4fc921d50763f6c4d9a
SpeedVan <61458673@qq.com>
2022-08-12 02:00:55 +0800

    none

--- crd/Controller.go
+++ crd/Controller.go
@@ -1,16 +1,30 @@
 package crd
 
 import (
-	"github.com/illublank/go-common/config"
-	"k8s.io/client-go/tools/cache"
+  "github.com/illublank/go-common/config"
+  "k8s.io/apimachinery/pkg/runtime/schema"
+  "k8s.io/client-go/tools/cache"
+  "k8s.io/client-go/tools/record"
 )
 
 type Controller struct {
-	CrdController
+  CrdController
+  recorder record.EventRecorder
+  informer cache.SharedIndexInformer
+  Store    cache.Store
 }
 
-func NewController(cfg config.Config, informer cache.SharedIndexInformer) *Controller {
+func NewController(cfg config.Config, gvr schema.GroupVersionResource, informer cache.SharedIndexInformer) *Controller {
 
-	informer.GetStore()
-	return nil
+  return &Controller{
+    informer: informer,
+    Store:    informer.GetStore(),
+  }
+}
+
+func (s *Controller) AddEventHandler(handlerFuncs cache.ResourceEventHandlerFuncs) *Controller {
+
+  s.informer.AddEventHandler(handlerFuncs)
+
+  return s
 }
